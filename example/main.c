39b15ac92657e09263ac273b471bb28e8480322b
Nima <67910202+mahdion-aau@users.noreply.github.com>
2020-12-18 15:06:38 +0330

    Revert "add existing file"
    This reverts commit 2db00e50a065021a2b35012cd4376efc8098f025.

--- example/main.c
+++ example/main.c
@@ -1,34 +1,49 @@
 /*
- * AES-128 Encryption in CBC Mode - Main File
+ * MIT License
+ *
+ * Copyright (c) 2018 Andres Amaya Garcia
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
  */
-#include "stdint.h"
-//#include "stdio.h" //printf
-#include "masked_combined.h"
-#include "rand.h"
-//#include "elmo-funcs.h"
 
-// Fixed 16 Byte Key:
-uint8_t key[16] = {0xb6, 0x10, 0x49, 0xfc, 0x9b, 0x73, 0xa0, 0xf5, 0x58, 0xcd, 0x6b, 0x38, 0xac, 0x29, 0xed, 0x00};
+/*
+ * This is not a fully compliant implementation of putchar() as it does not
+ * return the printed character on success and it does not set the error
+ * indicator as it is never expected to fail
+ */
+int putchar(int character);
 
-// Initializing lfsr 
-unsigned int rngSeed1 = 0x12FD01;
-unsigned int rngSeed2 = 0xF1AC14;
+int main()
+{
+    putchar('h');
+    putchar('e');
+    putchar('l');
+    putchar('l');
+    putchar('o');
+    putchar(' ');
+    putchar('w');
+    putchar('o');
+    putchar('r');
+    putchar('l');
+    putchar('d');
+    putchar('!');
+    putchar('\n');
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-int main(void) {
-
-    init_lfsrs(get_rand(), get_rand());
-    uint8_t ciphertext[16];
-    uint8_t plaintext[16]; 
-
-
-// Generating a random plaintext 
-    for(int i=0; i < 16; i++){
-        plaintext[i]= getRand();
-    }
-    add_to_trace(plaintext,16);
-    Encrypt(ciphertext, plaintext, key);
-    add_to_trace(plaintext,16);    
-    
+    return 0;
 }
-
